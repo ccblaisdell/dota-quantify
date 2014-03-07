17f13386999730b836a443016be1b2d6ecfc4162
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-03-07 12:55:53 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DotaQuantify::Application.config.secret_key_base = '6ce926a327e571cd9347e15e6545c19043a839445bbd49320a79fedd9fa10e8efe98855fcab3c23d93bb91ce270e5fba6bf4b42baa72362c8057f236cfb813e3'
