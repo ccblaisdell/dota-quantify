5abb400025ca51325c6d6e49d63cc844dee13a76
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-03-18 17:14:31 -0500

    Added dotabuff link on profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def dotabuff_link(target)
+    return link_to "View on dotabuff", dotabuff_url("players/#{target.dota_account_id}") if target.class == Profile
+    return link_to "View on dotabuff", dotabuff_url("matches/#{target.match_id}") if target.class == Match
+  end
+
+  def dotabuff_url(path)
+    "http://dotabuff.com/" + path
+  end
 end
