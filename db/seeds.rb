61ff474cea506b503d21e185de7f9874379c2570
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-03-19 23:06:41 -0500

    Created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,63 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Profile.create([
+  {
+    steam_account_id: 76561197966764637,
+    dota_account_id: 6498909,
+    name: "Dialuposaurus",
+    profile_url: "http://steamcommunity.com/profiles/76561197966764637/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/a7/a7ae68476e6b3594f84075241c5b344da52fb4c9.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/a7/a7ae68476e6b3594f84075241c5b344da52fb4c9_full.jpg",
+    follow: true
+  },{
+    steam_account_id: 76561198004693352,
+    dota_account_id: 44427624,
+    name: "hoplyte",
+    profile_url: "http://steamcommunity.com/profiles/76561198004693352/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/a2/a22faca966a2170cf1e500e21691415ec8fe717f.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/a2/a22faca966a2170cf1e500e21691415ec8fe717f_full.jpg",
+    follow: true
+  },{
+    steam_account_id: 76561198004538859,
+    dota_account_id: 44273131,
+    name: "mgrif",
+    profile_url: "http://steamcommunity.com/profiles/76561198004538859/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/8b/8be7f56decd5dc8b3340ab3e1c60c725a82d2576.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/8b/8be7f56decd5dc8b3340ab3e1c60c725a82d2576_full.jpg",
+    follow: true
+  },{
+    steam_account_id: 76561198065871366,
+    dota_account_id: 105605638,
+    name: "CharlyMurphyyy",
+    profile_url: "http://steamcommunity.com/profiles/76561198065871366/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/01/01c2c2298accb0a16fa1f4b4e806f0ff87349ba0.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/01/01c2c2298accb0a16fa1f4b4e806f0ff87349ba0_full.jpg",
+    follow: true
+  },{
+    steam_account_id: 76561198025766279,
+    dota_account_id: 65500551,
+    name: "losandro",
+    profile_url: "http://steamcommunity.com/id/losandro/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/ba/ba4517ddfd87375342464ca0b97ac5d5b666076a.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/ba/ba4517ddfd87375342464ca0b97ac5d5b666076a_full.jpg",
+    follow: true
+  },{
+    steam_account_id: 76561198031493697,
+    dota_account_id: 71227969,
+    name: "Gingarrr",
+    profile_url: "http://steamcommunity.com/profiles/76561198031493697/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/6b/6bc62b23dfebe7f2a5fddeb3e04d8a70e8474613.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/6b/6bc62b23dfebe7f2a5fddeb3e04d8a70e8474613_full.jpg",
+    follow: true
+  },{
+    steam_account_id: 76561198023924222,
+    dota_account_id: 63658494,
+    name: "Roamin Ronin",
+    profile_url: "http://steamcommunity.com/profiles/76561198023924222/",
+    small_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/81/811f1f99a43647cf770e8ca90a8ea4c20f785092.jpg",
+    big_avatar_url: "http://media.steampowered.com/steamcommunity/public/images/avatars/81/811f1f99a43647cf770e8ca90a8ea4c20f785092_full.jpg",
+    follow: true
+  }
+])
