dc3289858051ee2e84b7de2e6402e8e0406da294
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-03-28 17:35:40 -0500

    Removed other references to reports

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,6 @@ DotaQuantify::Application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'reports' => 'reports#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
