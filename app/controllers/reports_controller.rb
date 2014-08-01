b0bb2ea56e5eb5dd66e80136b782cf24a5bea986
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-07-31 19:37:36 -0500

    Added sortable players to report page

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,7 +2,16 @@ class ReportsController < ApplicationController
   def show
     find_report_period
     @matches = Match.between(@from, @to).by_date
-    @players = @matches.all.collect {|m| m.followed_players}.flatten
+
+    profile_ids = Profile.where(follow: true).pluck(:steam_account_id)
+    match_ids = @matches.pluck(:id)
+    @players = Player.where(
+        :steam_account_id.in => profile_ids, 
+        :match_id.in => match_ids)
+      .order_by([sort_column(Player, :start), sort_direction])
+      .page(params[:page])
+      
+    get_kda_max(@players)
   end
 
   private
