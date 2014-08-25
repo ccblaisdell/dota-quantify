781329dad367513ccaac23d13cd53d359b71418e
Colby Blaisdell <ccblaisdell@moxiemachine.com>
2014-08-25 10:28:30 -0500

    Same thing

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,5 +1,6 @@
 module PartiesHelper
   def party_link(party)
+    return if party.nil? or party.profiles.to_a.empty?
     link_to party, class: "party-link" do
       party.profiles.collect {|profile| avatar(profile)}.join(' ').html_safe
     end
